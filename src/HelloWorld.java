a13d86348d368ab7d706579fd3cf3eafd2da412a
c.josettelooft <cjlooft@gmail.com>
2018-09-17 15:21:17 -0500

    Working exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,157 @@
+//public class HelloWorld {
+//
+//    public static void main(String[] args) {
+//
+//        System.out.println("Hello, World!");
+//
+//    }
+//}
+//
+
+
 public class HelloWorld {
 
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, World!");
-    }
-}
+
+    public static void main(String[] args) {
+
+//
+//             int myFavoriteNumber;
+//             myFavoriteNumber = 8;
+//             System.out.println(myFavoriteNumber);
+//
+//
+//
+//             String myString = "There is no place like home.";
+//             System.out.println(myString);
+//
+//         }
+//     }
+
+//               Change your code to assign a character value to myString. What do you notice?
+//
+//             myString = "short goodbye";
+//
+//             System.out.println(myString);
+//         }
+//     }
+////
+//  Error:(34, 14) java: cannot find symbol
+//          symbol:   variable myString
+//          location: class HelloWorld
+//
+//Error:(36, 33) java: cannot find symbol
+//        symbol:   variable myString
+//        location: class HelloWorld
+
+//        Change your code to assign the value 3.14159 to myString. What happens?
+
+//        String myString = 3.14159;
+//     System.out.println(myString);
+//
+//             }
+//}
+//
+//Error:(50, 27) java: incompatible types: double cannot be converted to java.lang.String
+
+//      Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+
+
+//             long myNumber;
+//             System.out.print(myNumber);
+//         }
+//     }
+//
+//
+//Error:(62, 31) java: variable myNumber might not have been initialized
+
+
+////       Change your code to assign the value 3.14 to myNumber. What do you notice?
+//
+//             myNumber = 3.14;
+//             System.out.print(myNumber);
+//         }
+//     }
+//     Error:(72, 14) java: cannot find symbol
+//             symbol:   variable myNumber
+//             location: class HelloWorld
+//
+//Error:(73, 31) java: cannot find symbol
+//        symbol:   variable myNumber
+//        location: class HelloWorld
+
+
+//        Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+
+//             myNumber = 123L;
+//             System.out.println(myNumber);
+//         }
+//
+//     }
+//
+
+
+//        Change your code to assign the value 123 to myNumber.
+//
+//             myNumber = "123";
+//             System.out.println(myNumber);
+//         }
+//     }
+
+////       Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
+//
+////                Change your code to declare myNumber as a float. Assign the value 3.14 to it.
+//// What happens? What are two ways we could fix this?
+//
+//        float myNumber = (float) 3.14;
+//
+////               Copy and paste the following code blocks one at a time and execute them
+//
+////
+//             int x = 5;
+//             System.out.println(x++);
+//             System.out.println(x);
+//
+//         }
+//}
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//         }
+//         }
+///        What is the difference between the above code blocks? Explain why the code outputs what it does.
+//
+//                Try to create a variable named class. What happens?
+
+//             string class ="freshman";
+//
+//         }
+//     }
+
+
+//        Rewrite the following expressions using the relevant shorthand assignment operators:
+
+
+//
+//
+//    int x = 4;
+//    x = x + 5;
+////    x += 5;
+//
+////    int x = 3;
+////    int y = 4;
+////    y *= x;
+////
+//
+//
+//           int x =10
+//       int y = 2;
+//       x = x / y;
+//      x /= y;
+//      y -= x;
+//   }
+//
+//    }
+//}
+
+         
